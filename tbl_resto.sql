1eda543b3ab347e23b04668bb86b68b9d6144fc9
Hectoplasma <97678494+Hectoplasma@users.noreply.github.com>
2024-11-29 00:17:10 +0700

    Add files via upload

--- tbl_resto.sql
+++ tbl_resto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2024 at 12:56 PM
+-- Generation Time: Nov 28, 2024 at 06:16 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -46,8 +46,8 @@ CREATE TABLE `tbl_resto` (
 
 INSERT INTO `tbl_resto` (`id_resto`, `nama_resto`, `jenis_resto`, `jenis_makanan`, `jam_buka`, `jam_tutup`, `lokasi`, `foto`, `latitude`, `longitude`) VALUES
 (101, 'Warung Dayat', 'Warung', 'Sarapan', '06:00:00', '14:00:00', 'Sekupang', '1.jpg', '1.121886', '103.967820'),
-(102, 'Warung Endul', 'Warung', 'Makan Berat', '09:00:00', '15:00:00', 'Sekupang', '2.jpg', '1.1218776', '103.9678076'),
-(103, 'Gorengan Kliwon', 'Gerobak', 'Gorengan', '13:00:00', '20:00:00', 'Sekupang', '3.jpg', '1.1218642', '103.9695075'),
+(102, 'Warung Endul', 'Warung', 'Makanan Berat', '09:00:00', '15:00:00', 'Sekupang', '2.jpg', '1.1218776', '103.9678076'),
+(103, 'Gorengan Kliwon', 'Gerobak', 'Makanan Ringan', '13:00:00', '20:00:00', 'Sekupang', '3.jpg', '1.1218642', '103.9695075'),
 (104, 'Bakso Bom', 'Gerobak', 'Makanan Berat', '10:00:00', '20:00:00', 'Sekupang', '4.jpg', '1.1210163', '103.9695568'),
 (105, 'Ampera Gita', 'Warung', 'Makanan Berat', '10:00:00', '20:00:00', 'Sekupang', '5.jpg', '1.1234906', '103.9689291'),
 (106, 'Pak Eko Fried Chicken', 'Gerobak', 'Makanan Berat', '09:00:00', '20:00:00', 'Sekupang', '6.jpg', '1.1237021', '103.9688312'),
@@ -57,7 +57,7 @@ INSERT INTO `tbl_resto` (`id_resto`, `nama_resto`, `jenis_resto`, `jenis_makanan
 (110, 'Bude Longbud', 'Warung', 'Makanan Berat', '12:00:00', '20:00:00', 'Sekupang', '10.jpg', '1.1218769', '103.9726842'),
 (111, 'RM Pendowo', 'Warung', 'Makanan Berat', '11:00:00', '17:00:00', 'Sekupang', '11.jpg', '1.1098719', '103.9654168'),
 (112, 'Mie Ayam dan Bakso 89', 'Warung', 'Makanan Berat', '06:00:00', '20:30:00', 'Sekupang', '12.jpg', '1.1098089', '103.9653923'),
-(113, 'Mie Ayam dan Bakso Prasmanan Osing', 'Ruko', 'Makanan Berat', '10:00:00', '21:00:00', 'Sekupang', '13.jpg', '1,1103754', '103.9619903'),
+(113, 'Bakso dan Mie Ayam Prasmanan Osing ', 'Ruko', 'Makanan Berat', '10:00:00', '21:00:00', 'Sekupang', '13.jpg', '1.1103754\r\n', '103.9619903'),
 (114, 'Batagor dan Siomay Nisa', 'Gerobak', 'Makanan Ringan', '15:00:00', '21:00:00', 'Sekupang', '14.jpg', '1.0987123', '103.9324856'),
 (115, 'RM Ayuk Lina', 'Warung', 'Makanan Berat', '09:00:00', '17:00:00', 'Sekupang', '15.jpg', '1.1036537', '103.9493007'),
 (116, 'Ampera Santan Balado', 'Warung', 'Makanan Berat', '09:00:00', '17:00:00', 'Sekupang', '16.jpg', '1.1031864', '103.9526609'),
@@ -69,7 +69,14 @@ INSERT INTO `tbl_resto` (`id_resto`, `nama_resto`, `jenis_resto`, `jenis_makanan
 (122, 'RM Minahasa', 'Ruko', 'Makanan Berat', '09:00:00', '18:00:00', 'Bengkong', '22.jpg', '1.1606452', '104.0178067'),
 (123, 'Bakso Persegi', 'Warung', 'Makanan Berat', '14:00:00', '01:00:00', 'Bengkong', '23.jpg', '1.1586886', '104.0284715'),
 (124, 'Warung Yuen', 'Warung', 'Makanan Berat', '11:00:00', '22:00:00', 'Bengkong', '24.jpg', '1.1603244', '104.0347888'),
-(125, 'RM Mbak Ningsih', 'Ruko', 'Makanan Berat', '10:00:00', '22:00:00', 'Bengkong', '25.jpg', '1.1641445', '104.0360004');
+(125, 'RM Mbak Ningsih', 'Ruko', 'Makanan Berat', '10:00:00', '22:00:00', 'Bengkong', '25.jpg', '1.1641445', '104.0360004'),
+(126, 'Ayam Penyet Azka', 'Gerobak', 'Makanan Berat', '15:00:00', '23:00:00', 'Bengkong', '26.jpg', '1.1626779', '104.0358164'),
+(127, 'Zhengda Chicken Steak', 'Ruko', 'Makanan Berat', '09:00:00', '21:00:00', 'Bengkong', '27.jpg', '1.140412', '104.037090'),
+(128, 'Morning Kopitiam', 'Ruko', 'Makanan Berat', '06:00:00', '15:00:00', 'Bengkong', '28.jpg', '1.143601', '104.036933'),
+(129, 'Kedai Kopi Aso', 'Ruko', 'Makanan Berat', '06:00:00', '18:00:00', 'Bengkong', '29.jpg', '1.143868', '104.036933'),
+(130, 'Coffee YO\\XZ', 'Ruko', 'Makanan Berat', '06:00:00', '18:00:00', 'Bengkong', '30.jpg', '1.142048', '104.038244'),
+(131, 'Bakso Ujang Mak Inyong', 'Warung', 'Makanan Berat', '06:00:00', '18:00:00', 'Bengkong', '31.jpg', '1.141601', '104.038175'),
+(132, 'Pempek Cek Eka', 'Gerobak', 'Makanan Ringan', '09:00:00', '21:00:00', 'Bengkong', '32.jpg', '1.152389', '104.038332');
 
 --
 -- Indexes for dumped tables
